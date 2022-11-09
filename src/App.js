a76476fb966e5f8c3f76773fbf6e18273837bf43
timisiulia <timis_iulia1@yahoo.com>
2022-11-09 17:50:50 +0200

    last changes of frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/Login";
+import AdminPage from "./pages/AdminPage";
+import CreateUserPage from "./pages/CreateUserPage";
+import CreateDevicePage from "./pages/CreateDevicePage";
+import ViewUsersPage from "./pages/ViewUsersPage";
+import UpdateUserPage from "./pages/UpdateUserPage";
+import ViewDevicePage from "./pages/ViewDevicePage";
+import UpdateDevicePage from "./pages/UpdateDevicePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/admin-page" element={<AdminPage />} />
+            <Route path="/user-page" element={<CreateUserPage />} />
+            <Route path="/device-page" element={<CreateDevicePage />} />
+            <Route path="/view-users" element={<ViewUsersPage />} />
+            <Route path="/update-page" element={<UpdateUserPage />} />
+            <Route path="/view-device" element={<ViewDevicePage />} />
+            <Route path="/update-page-device" element={<UpdateDevicePage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
